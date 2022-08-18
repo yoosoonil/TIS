e0a504051213941332c0d4f158081f924f8c3a49
yoosoonil <y2k7654@gmail.com>
2022-08-18 23:30:46 +0900

    220818

---
+++ Backjoon/220818_1110_더하기사이클.py
@@ -0,0 +1,16 @@
+n = int(input()) # 68
+
+num = n
+cycle = 0
+
+while True:
+    a = num // 10       # 10으로 나눠서 몫 = 6
+    b = num % 10        # 10으로 나눠서 나머지 = 8
+    c = (a + b) % 10    # 10으로 나눠서 나머지 = 4
+    num = (b * 10) + c  # 84
+    
+    cycle += 1
+    if num == n:        # num이 처음 입력 값 n과 같아지면 break
+        break
+
+print(cycle)
