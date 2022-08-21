4ba3a7e89410e95066a9eddd869a8ad0f297bd27
yoosoonil <y2k7654@gmail.com>
2022-08-22 00:21:38 +0900

    220821

--- Backjoon/220821_17249_태보태보.py
+++ Backjoon/220821_17249_태보태보.py
@@ -7,6 +7,5 @@ for c in a:
     if c == '@':
         r[i] += 1
     elif  c == '(':
-        i ^= 1          # ^= 뭐임? 1로 고정한다는 것 같은데 뭐지?
-    print(i)
-# print(*r)       # '*'은 unpacking임. 리스트를 그냥 출력 하는 것은 '*' 이다. ex) print(*[1,2,3]) -> 1 2 3
+        i ^= 1        # ^= XOR 연산자임. XOR 연산자를 공부하도록.
+print(*r)       # '*'은 unpacking임. 리스트를 그냥 출력 하는 것은 '*' 이다. ex) print(*[1,2,3]) -> 1 2 3
