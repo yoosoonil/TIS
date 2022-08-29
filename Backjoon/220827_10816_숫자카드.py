391a0323a3cc8a50b578e3b237cc678e0bc1a4ab
yoosoonil <y2k7654@gmail.com>
2022-08-29 22:54:19 +0900

    220827_푸는중

---
+++ Backjoon/220827_10816_숫자카드.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline  # line 들어가는대로 입력
+
+N = int(input())
+cards = sorted([*map(int, input().split())])    # 오름차순으로 정렬
+M = int(input())
+candidate = [*map(int, input().split())]
+
+count = {}
+for card in cards:
+    if card in count:
+        count[card] += 1
+    else:
+        count[card] = 1
+
+def binarySearch(arr, target, start, end):
+    if start > end:
+        return 0
+    
+    mid = (start + end) // 2
+    if arr[mid] == target:
+        return count.get(target)
+    elif arr[mid] < target:
+        return binarySearch(arr, target, mid+1, end)
+    else:
+        return binarySearch(arr, target, start, mid-1)
+    
+for target in candidate:
+    print(binarySearch(cards, target, 0, len(cards)-1), end=" ")
