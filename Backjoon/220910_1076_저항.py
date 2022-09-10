d8f72edc49f47fec6a88dd925aaab57dde6a6f45
yoosoonil <y2k7654@gmail.com>
2022-09-10 23:38:02 +0900

    220910_2

---
+++ Backjoon/220910_1076_저항.py
@@ -0,0 +1,20 @@
+a = input()
+b = input()
+c = input()
+
+resist = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue':6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9    
+}
+# 값들을 딕셔너리로 만든다.
+
+print((resist[a]*10 + resist[b]) * (10 ** resist[c]))
+# 앞자리+뒷자리 * 10^c 이기에 (a*10+b)*10**c 이다.
