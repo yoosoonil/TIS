9864b232d362a815fcc87f704037e889fb383059
yoosoonil <y2k7654@gmail.com>
2022-09-11 23:49:16 +0900

    220911_1_풀이공부필요

---
+++ Backjoon/220911_20001_고무오리.py
@@ -0,0 +1,21 @@
+# 문제풀이 참고 : https://ywtechit.tistory.com/206
+
+stack = []
+ 
+while True:
+    s = input()
+    if s == '문제':
+        stack.append(1)
+    elif s == '고무오리':
+        if not stack:
+            stack.append(1)
+            stack.append(1)
+        else:
+            stack.pop()
+    elif s == '고무오리 디버깅 끝':
+        break
+ 
+if not stack:
+    print('고무오리야 사랑해')
+else:
+    print('힝구')
